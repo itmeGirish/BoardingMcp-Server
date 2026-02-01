bbfb988dad89c253332c0d7fbc6659f378ad2557
GirishN <Girish.N@fusionfoundry.com>
2026-02-02 00:08:03 +0530

    020222026

--- app/services/image_service.py
+++ app/services/image_service.py
@@ -2,3 +2,9 @@
 image_service.py - Auto-generated
 Implement your logic here
 """
+from langchain_openai import ChatOpenAI
+from ..config import settings,logger
+
+llm = ChatOpenAI(api_key=settings.OPENAI_API_KEY,)
+
+
